a6767d18992308671079e0dec1a06140c4f77197
Andrew <alekseevandrew105@gmail.com>
2023-08-17 13:04:46 +0300

    homework19

---
+++ src/Lesson39.java
@@ -0,0 +1,13 @@
+import Lesson38.ScannerException;
+
+public class Lesson39 {
+    public static void main(String[] args) {
+        try {
+            int[] arr = new int[2];
+        } catch (ArrayIndexOutOfBoundsException e) {
+
+        }
+
+
+    }
+}
